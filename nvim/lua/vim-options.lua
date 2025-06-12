8dd60fcd7954d7a70c285990e0f658992935bc07
TanDuy18 <tanduy18.it@gmail.com>
2025-06-12 09:20:47 +0700

    done search

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,20 +4,18 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.cmd("set number")
-vim.cmd("set relativenumber")
+vim.cmd("set norelativenumber")
 vim.cmd("set cursorline")
-vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "white" })
-vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#ead84e" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ead84e", bold = true })     -- Số dòng đang đứng
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#808080"})       -- Các số dòng còn lại
 vim.api.nvim_set_option("clipboard", "unnamed")
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
--- move selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- paste over highlight word
+vim.opt.swapfile = false
 vim.keymap.set("x", "<leader>p", '"_dP')
-vim.opt.colorcolumn = "94"
--- fk llm-ls
+vim.keymap.set("n","E","$")
 local notify_original = vim.notify
 vim.notify = function(msg, ...)
 	if
@@ -32,4 +30,7 @@ vim.notify = function(msg, ...)
 	end
 	return notify_original(msg, ...)
 end
+
+
+
 vim.opt.swapfile = false
